0d827b0321bf0efaedb8782260ef80c93bd0c9ac
AlexW-project <alexanderw124@nycstudents.net>
2026-01-22 15:33:53 +0000

    123

--- farm.js
+++ farm.js
@@ -1,14 +1,24 @@
- class Barn{
-     constructor(x,y,z){
-         this.x = x;
-         this.y = y;
-         this.z = z;
-         this.obj = document.createElement('a-entity');
-         
-         let barn = document.createElement('a-box');
-         this.obj.append(barn)
+ class Barn {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
 
-         scene.append(this.obj);
-     }
+        // Get the A-Frame scene
+        const scene = document.querySelector('a-scene');
 
+        // Parent entity
+        this.obj = document.createElement('a-entity');
+        this.obj.setAttribute('position', `${x} ${y} ${z}`);
+
+        // Barn body
+        const barn = document.createElement('a-box');
+        barn.setAttribute('width', 4);
+        barn.setAttribute('height', 3);
+        barn.setAttribute('depth', 4);
+        barn.setAttribute('color', '#8B0000'); // barn red
+
+        this.obj.appendChild(barn);
+        scene.appendChild(this.obj);
+    }
 }
